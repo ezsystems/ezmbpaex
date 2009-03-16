b52090ecc0d1ae245e6729f7010b1ca80c9ce2f3
Nicolas Pastorino <nfrp@ez.no>
2009-03-16 10:13:22 +0000

    --updated headers.
    git-svn-id: http://svn.projects.ez.no/mbpaex/trunk@5 cb9d5724-9aaa-413f-8b5f-45cb6511e0f6

--- cronjobs/sendexpirynotifications.php
+++ cronjobs/sendexpirynotifications.php
@@ -4,7 +4,7 @@
  *
  * @copyright Copyright (C) 1999-2008 eZ Systems AS. All rights reserved.
  * @license http://ez.no/licenses/gnu_gpl GNU GPLv2
- * @package mbpaex
+ * @package ezmbpaex
  */
 
 $cli->output( "eZPaEx: Send expiry notifications process start" );

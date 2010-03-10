168cdd71b467b0b935fbb3dbbdfd6e2b500dbf8a
Nicolas Pastorino <nfrp@ez.no>
2010-03-10 10:53:14 +0000

    --Brazilian translations, credits go to Renan Leme
    git-svn-id: http://svn.projects.ez.no/mbpaex/trunk@32 cb9d5724-9aaa-413f-8b5f-45cb6511e0f6

---
+++ translations/por-BR/translation.ts
@@ -0,0 +1,199 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>mbpaex/classes/datatypes</name>
+    <message>
+        <source>Password Expiration</source>
+        <comment>Datatype name</comment>
+        <translation>Expiração de senha</translation>
+    </message>
+    <message>
+        <source>The password lifetime must be an integer &gt;= 0</source>
+        <translation>O tempo de vida da senha deve ser um número inteiro &gt;= 0</translation>
+    </message>
+    <message>
+        <source>The expiration notification time must be an integer &gt;= 86400</source>
+        <translation>O tempo de notificação de expiração da senha deve ser um número inteiro &gt;= 86400</translation>
+    </message>
+    <message>
+        <source>The password doesn&apos;t match the validation rule.</source>
+        <translation>A senha não coincide com a regra de validação.</translation>
+    </message>
+    <message>
+        <source>Wrong value in updatechildren field</source>
+        <translation>Valor incorreto para o campo updatechildren</translation>
+    </message>
+</context>
+<context>
+    <name>mbpaex/content/datatype</name>
+    <message>
+        <source>Validation Regexp</source>
+        <translation>Regex de Validação</translation>
+    </message>
+    <message>
+        <source>Password LifeTime (DAYS)</source>
+        <translation>Tempo de vida da senha (DIAS)</translation>
+    </message>
+    <message>
+        <source>Expiration Notification (SECONDS)</source>
+        <translation>Notificação de Expiração (SEGUNDOS)</translation>
+    </message>
+    <message>
+        <source>Current password status:</source>
+        <translation>Status atual da senha:</translation>
+    </message>
+    <message>
+        <source>expired</source>
+        <translation>expirada</translation>
+    </message>
+    <message>
+        <source>active</source>
+        <translation>ativa</translation>
+    </message>
+    <message>
+        <source>Update children nodes?</source>
+        <translation>Atualizar nós filhos?</translation>
+    </message>
+    <message>
+        <source>undefined</source>
+        <translation>indefinido</translation>
+    </message>
+    <message>
+        <source>Password status</source>
+        <translation>Status da senha</translation>
+    </message>
+    <message>
+        <source>Children update pending, will be done in a few minutes...</source>
+        <translation>Atualização de nós filhos pendente, será feito em poucos minutos...</translation>
+    </message>
+</context>
+<context>
+    <name>mbpaex/userpaex</name>
+    <message>
+        <source>Change password for user</source>
+        <translation>Trocar senha de usuario</translation>
+    </message>
+    <message>
+        <source>Please retype your old password.</source>
+        <translation>Insira sua senha antiga.</translation>
+    </message>
+    <message>
+        <source>Password didn&apos;t match, please retype your new password.</source>
+        <translation>As novas senhas não coincidem, por favor redigite as novas senhas.</translation>
+    </message>
+    <message>
+        <source>Password didn&apos;t validate, please retype your new password.</source>
+        <translation>A senha não é válida, por favor redigite sua nova senha.</translation>
+    </message>
+    <message>
+        <source>Password successfully updated.</source>
+        <translation>A senha foi modificada com sucesso.</translation>
+    </message>
+    <message>
+        <source>Old password</source>
+        <translation>Senha antiga</translation>
+    </message>
+    <message>
+        <source>New password</source>
+        <translation>Nova senha</translation>
+    </message>
+    <message>
+        <source>Retype password</source>
+        <translation>Redigite a senha</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Cancelar</translation>
+    </message>
+    <message>
+        <source>Your password is about to expire</source>
+        <translation>Sua senha está prestes a expirar</translation>
+    </message>
+    <message>
+        <source>%siteurl your password is about to expire</source>
+        <translation>%siteurl sua senha está prestes a expirar</translation>
+    </message>
+    <message>
+        <source>New password must be different from the old one. Please choose another password.</source>
+        <translation>A nova senha deve ser diferente da antiga. Por favor, escolha outra senha.</translation>
+    </message>
+</context>
+<context>
+    <name>mbpaex/userpaex/forgotpassword</name>
+    <message>
+        <source>A mail has been sent to the following email address: %1. This email contains a link you need to click so that we can confirm that the correct user is getting the new password.</source>
+        <translation>Um e-mail foi enviado para o endereço de e-mail %1. O e-mail contém um link no qual você deve clicar para confirmar que o usuário correto está recebendo a nova senha.</translation>
+    </message>
+    <message>
+        <source>There is no registered user with that email address.</source>
+        <translation>Não existe nenhum usuário registrado com esse endereço de e-mail.</translation>
+    </message>
+    <message>
+        <source>The password has been changed successfully.</source>
+        <translation>A senha foi modificada com sucesso.</translation>
+    </message>
+    <message>
+        <source>The passwords do not match. Please, be sure to enter the same password in both fields.</source>
+        <translation>As novas senhas não são idênticas. Por favor, confira se as novas senhas são idênticas.</translation>
+    </message>
+    <message>
+        <source>The new password is invalid, please choose new one.</source>
+        <translation>A nova senha é inválida, por favor escolha uma nova.</translation>
+    </message>
+    <message>
+        <source>Enter your desired new password in the form below.</source>
+        <translation>Informe sua nova senha no formulário a seguir.</translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation>Senha</translation>
+    </message>
+    <message>
+        <source>Password Confirm</source>
+        <translation>Confirmação de senha</translation>
+    </message>
+    <message>
+        <source>Change password</source>
+        <translation>Mudar senha</translation>
+    </message>
+    <message>
+        <source>The key is invalid or has been used. </source>
+        <translation>A chave é inválida ou já foi utilizada. </translation>
+    </message>
+    <message>
+        <source>Have you forgotten your password?</source>
+        <translation>Esqueceu sua senha?</translation>
+    </message>
+    <message>
+        <source>If you have forgotten your password we can generate a new one for you. All you need to do is to enter your email address and we will create a new password for you.</source>
+        <translation>Se você esqueceu sua senha, uma nova pode ser gerada. Digite seu endereço de e-mail e uma nova senha será enviada a você.</translation>
+    </message>
+    <message>
+        <source>Email</source>
+        <translation>Email</translation>
+    </message>
+    <message>
+        <source>Generate new password</source>
+        <translation>Gerar nova senha</translation>
+    </message>
+    <message>
+        <source>%siteurl forgot password</source>
+        <translation>%siteurl senha esquecida</translation>
+    </message>
+    <message>
+        <source>Click here to set your new password</source>
+        <translation>Clique aqui para definir sua nova senha</translation>
+    </message>
+    <message>
+        <source>This link is valid until %1, after that time you should generate a new one.</source>
+        <translation>Este link é válido até %1, após esse prazo você deve gerar um novo.</translation>
+    </message>
+    <message>
+        <source>Choose a new password</source>
+        <translation>Escolha uma nova senha</translation>
+    </message>
+</context>
+</TS>

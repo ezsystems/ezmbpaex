0d9954f043c6f0e4f9a3f07707b42fe5f028cf6a
Jérôme Renard <jr@ez.no>
2010-03-24 10:41:58 +0000

    - Updated copyright notice with current year
    git-svn-id: http://svn.projects.ez.no/mbpaex/trunk@33 cb9d5724-9aaa-413f-8b5f-45cb6511e0f6

--- ezinfo.php
+++ ezinfo.php
@@ -2,7 +2,7 @@
 /**
  * File containing the ezmbpaexInfo class
  *
- * @copyright Copyright (C) 1999-2009 eZ Systems AS. All rights reserved.
+ * @copyright Copyright (C) 1999-2010 eZ Systems AS. All rights reserved.
  * @license http://ez.no/licenses/gnu_gpl GNU GPLv2
  * @package ezmbpaex
  */
@@ -13,7 +13,7 @@ class ezmbpaexInfo
     {
         return array( 'Name'      => 'eZ Password Expiry',
                       'Version'   => '1.2.0',
-                      'Copyright' => 'Copyright (C) 2009 eZ Systems AS',
+                      'Copyright' => 'Copyright (C) 1999-2010 eZ Systems AS',
                       'License'   => 'GNU General Public License v2.0',
                       'Credits'   => '<a href="http://www.microblau.net/">Microblau</a> and <a href="http://www.mitsue.co.jp">Mitsue-Links Co. Ltd.</a>, two dedicated eZ Partners.'
                     );

edb6ca437b7d0028f9190422dc78876ff48bbcb8
André Rømcke <ar@ez.no>
2010-11-11 14:50:04 +0100

    Make sure userpaex/password & forgotpassword is available in admin interface
    (When not logged in)

--- settings/site.ini.append.php
+++ settings/site.ini.append.php
@@ -3,7 +3,7 @@
 [UserSettings]
 ExtensionDirectory[]=ezmbpaex
 
-# Reset array to disable standard login handler that bypass the 
+# Reset array to disable standard login handler that bypass the
 # paex validation
 LoginHandler[]
 LoginHandler[]=paex
@@ -11,11 +11,16 @@ LoginHandler[]=paex
 [RegionalSettings]
 TranslationExtensions[]=ezmbpaex
 
-# Allow anonymous access to change password form for users with 
+# Allow anonymous access to change password form for users with
 # expired password to change it without log into the system and
 # to forgotpassword process for users get a new forgot password.
 [RoleSettings]
 PolicyOmitList[]=userpaex/password
 PolicyOmitList[]=userpaex/forgotpassword
+
+
+[SiteAccessSettings]
+AnonymousAccessList[]=userpaex/password
+AnonymousAccessList[]=userpaex/forgotpassword
 
 */ ?>

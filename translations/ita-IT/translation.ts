f8535ddd494ba3085f18afa50c683bc32851a272
netbliss <sandra@netbliss.eu>
2018-10-03 17:21:08 +0200

    Italian translation created (#5)
    Created italian translation

---
+++ translations/ita-IT/translation.ts
@@ -0,0 +1,359 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="it_IT">
+<context>
+    <name>design/admin/content/edit</name>
+    <message>
+        <source>View and manage (copy, delete, etc.) the versions of this object.</source>
+        <translation>Guarda e gestisci (copia, elimina, ecc.) le versioni di quest&apos;oggetto.</translation>
+    </message>
+    <message>
+        <source>Manage versions</source>
+        <translation>Gestisci versioni</translation>
+    </message>
+</context>
+<context>
+    <name>design/admin/node/view/full</name>
+    <message>
+        <source>Last modified</source>
+        <translation>Ultima modifica</translation>
+    </message>
+    <message>
+        <source>Node ID</source>
+        <translation>ID nodo</translation>
+    </message>
+    <message>
+        <source>Object ID</source>
+        <translation>ID oggetto</translation>
+    </message>
+    <message>
+        <source>Another language</source>
+        <translation>Un&apos;altra lingua</translation>
+    </message>
+    <message>
+        <source>Edit</source>
+        <translation>Modifica</translation>
+    </message>
+    <message>
+        <source>Edit the contents of this item.</source>
+        <translation>Modifica i contenuti di quest&apos;elemento.</translation>
+    </message>
+    <message>
+        <source>Not available</source>
+        <translation>Non disponibile</translation>
+    </message>
+    <message>
+        <source>You do not have permission to edit this item.</source>
+        <translation>Non sei abilitato a modifcare quest&apos;elemento.</translation>
+    </message>
+    <message>
+        <source>Move</source>
+        <translation>Sposta</translation>
+    </message>
+    <message>
+        <source>Move this item to another location.</source>
+        <translation>Sposta quest&apos;elemento in un&apos;altra collocazione.</translation>
+    </message>
+    <message>
+        <source>You do not have permission to move this item to another location.</source>
+        <translation>Non sei abilitato a spostare quest&apos;elemento in un&apos;altra collocazione.</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>Elimina</translation>
+    </message>
+    <message>
+        <source>Remove this item.</source>
+        <translation>Elimina quest&apos;elemento.</translation>
+    </message>
+    <message>
+        <source>You do not have permission to remove this item.</source>
+        <translation>Non sei abilitato ad eliminare quest&apos;elemento.</translation>
+    </message>
+    <message>
+        <source>Preview</source>
+        <translation>Anteprima</translation>
+    </message>
+    <message>
+        <source>New translation</source>
+        <translation>Nuova traduzione</translation>
+    </message>
+</context>
+<context>
+    <name>design/admin/pagelayout</name>
+    <message>
+        <source>Current user</source>
+        <translation>Utente attuale</translation>
+    </message>
+    <message>
+        <source>Change name, email, password, etc.</source>
+        <translation>Cambia nome, email, password, ecc.</translation>
+    </message>
+    <message>
+        <source>Change information</source>
+        <translation>Cambia informazioni</translation>
+    </message>
+    <message>
+        <source>Change user info</source>
+        <translation>Cambia info utente</translation>
+    </message>
+    <message>
+        <source>Change password for &lt;%username&gt;.</source>
+        <translation>Cambia password per &lt;%username&gt;.</translation>
+    </message>
+    <message>
+        <source>Change password</source>
+        <translation>Cambia password</translation>
+    </message>
+    <message>
+        <source>There is %basket_count item in the shopping basket.</source>
+        <translation>Vi è %basket_count elemento nel carrello.</translation>
+    </message>
+    <message>
+        <source>Shopping basket (%basket_count)</source>
+        <translation>Carrello (%basket_count)</translation>
+    </message>
+    <message>
+        <source>There are %basket_count items in the shopping basket.</source>
+        <translation>Vi sono %basket_count elementi nel carrello.</translation>
+    </message>
+    <message>
+        <source>Logout from the system.</source>
+        <translation>Logout dal sistema.</translation>
+    </message>
+    <message>
+        <source>Logout</source>
+        <translation>Logout</translation>
+    </message>
+</context>
+<context>
+    <name>design/standard/user/forgotpassword</name>
+    <message>
+        <source>Your account information</source>
+        <translation>Informazioni sul tuo account</translation>
+    </message>
+    <message>
+        <source>Email</source>
+        <translation>Email</translation>
+    </message>
+</context>
+<context>
+    <name>kernel/user</name>
+    <message>
+        <source>User</source>
+        <translation>Utente</translation>
+    </message>
+    <message>
+        <source>Forgot password</source>
+        <translation>Password dimenticata</translation>
+    </message>
+    <message>
+        <source>Change password</source>
+        <translation>Cambia password</translation>
+    </message>
+</context>
+<context>
+    <name>kernel/user/register</name>
+    <message>
+        <source>Registration info</source>
+        <translation>Info registrazione</translation>
+    </message>
+</context>
+<context>
+    <name>mbpaex/classes/datatypes</name>
+    <message>
+        <source>Password Expiration</source>
+        <comment>Datatype name</comment>
+        <translation>Scadenza password</translation>
+    </message>
+    <message>
+        <source>The password lifetime must be an integer &gt;= 0</source>
+        <translation>La durata della password dev&apos;essere un intero &gt;= 0</translation>
+    </message>
+    <message>
+        <source>The expiration notification time must be an integer &gt;= 86400</source>
+        <translation>L&apos;intervallo di tempo di notifica della scadenza dev&apos;essere un intero &gt;= 86400</translation>
+    </message>
+    <message>
+        <source>The password doesn&apos;t match the validation rule.</source>
+        <translation>La password non coincide con la validazione.</translation>
+    </message>
+    <message>
+        <source>Wrong value in updatechildren field</source>
+        <translation>Valore sbagliato nel campo aggiorna figli</translation>
+    </message>
+</context>
+<context>
+    <name>mbpaex/content/datatype</name>
+    <message>
+        <source>Validation Regexp</source>
+        <translation>Validazione Regexp</translation>
+    </message>
+    <message>
+        <source>Password LifeTime (DAYS)</source>
+        <translation>Durata password (GIORNI)</translation>
+    </message>
+    <message>
+        <source>Expiration Notification (SECONDS)</source>
+        <translation>Notifica scadenza (SECONDI)</translation>
+    </message>
+    <message>
+        <source>Current password status:</source>
+        <translation>Stato password attuale:</translation>
+    </message>
+    <message>
+        <source>expired</source>
+        <translation>scaduta</translation>
+    </message>
+    <message>
+        <source>active</source>
+        <translation>attiva</translation>
+    </message>
+    <message>
+        <source>Update children nodes?</source>
+        <translation>Aggiornare i nodi figli?</translation>
+    </message>
+    <message>
+        <source>undefined</source>
+        <translation>indefinito</translation>
+    </message>
+    <message>
+        <source>Password status</source>
+        <translation>Stato password</translation>
+    </message>
+    <message>
+        <source>Children update pending, will be done in a few minutes...</source>
+        <translation>Aggiornamento figli in attesa, verrà eseguito in pochi minuti...</translation>
+    </message>
+</context>
+<context>
+    <name>mbpaex/userpaex</name>
+    <message>
+        <source>Change password for user</source>
+        <translation>Cambia la password per l&apos;utente</translation>
+    </message>
+    <message>
+        <source>Please retype your old password.</source>
+        <translation>Sei pregato di reinserire la tua vecchia password.</translation>
+    </message>
+    <message>
+        <source>Password didn&apos;t match, please retype your new password.</source>
+        <translation>Le password non coincidono, sei pregato di reinserire la tua nuova password.</translation>
+    </message>
+    <message>
+        <source>Password didn&apos;t validate, please retype your new password.</source>
+        <translation>La password non è valida, sei pregato di reinserire la tua nuova password.</translation>
+    </message>
+    <message>
+        <source>New password must be different from the old one. Please choose another password.</source>
+        <translation>La nuova password dev&apos;essere diversa dalla vecchia. Sei pregato di scegliere un&apos;altra password.</translation>
+    </message>
+    <message>
+        <source>Password successfully updated.</source>
+        <translation>Aggiornamento password riuscito.</translation>
+    </message>
+    <message>
+        <source>Old password</source>
+        <translation>Vecchia password</translation>
+    </message>
+    <message>
+        <source>New password</source>
+        <translation>Nuova password</translation>
+    </message>
+    <message>
+        <source>Retype password</source>
+        <translation>Reinserisci la password</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Annulla</translation>
+    </message>
+    <message>
+        <source>%siteurl your password is about to expire</source>
+        <translation>%siteurl la tua password sta per scadere</translation>
+    </message>
+    <message>
+        <source>Your password is about to expire</source>
+        <translation>La tua password sta per scadere</translation>
+    </message>
+</context>
+<context>
+    <name>mbpaex/userpaex/forgotpassword</name>
+    <message>
+        <source>Click here to set your new password</source>
+        <translation>Clicca qui per impostare la tua nuova password</translation>
+    </message>
+    <message>
+        <source>A mail has been sent to the following email address: %1. This email contains a link you need to click so that we can confirm that the correct user is getting the new password.</source>
+        <translation>E&apos; stata inviata un&apos;email al seguente indirizzo email: %1. Quest&apos;email contiene un link che devi cliccare per darci la conferma che l&apos;utente giusto ha ricevuto la nuova password.</translation>
+    </message>
+    <message>
+        <source>There is no registered user with that email address.</source>
+        <translation>Non esiste alcun utente registrato con quest&apos;indirizzo email.</translation>
+    </message>
+    <message>
+        <source>The password has been changed successfully.</source>
+        <translation>Modifica password riuscita.</translation>
+    </message>
+    <message>
+        <source>The passwords do not match. Please, be sure to enter the same password in both fields.</source>
+        <translation>Le password non coincidono. Sei pregato di verificare di aver inserito la stessa password in entrambi i campi.</translation>
+    </message>
+    <message>
+        <source>The new password is invalid, please choose new one.</source>
+        <translation>La nuova password non è valida, sei pregato di sceglierne un&apos;altra.</translation>
+    </message>
+    <message>
+        <source>Choose a new password</source>
+        <translation>Scegli una nuova password</translation>
+    </message>
+    <message>
+        <source>Enter your desired new password in the form below.</source>
+        <translation>Inserisci la nuova password scelta nel campo sottostante.</translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation>Password</translation>
+    </message>
+    <message>
+        <source>Password Confirm</source>
+        <translation>Conferma password</translation>
+    </message>
+    <message>
+        <source>Change password</source>
+        <translation>Cambia password</translation>
+    </message>
+    <message>
+        <source>The key is invalid or has been used. </source>
+        <translation>La chiave non è valida o è stata usata.</translation>
+    </message>
+    <message>
+        <source>Have you forgotten your password?</source>
+        <translation>Hai dimenticato la tua password?</translation>
+    </message>
+    <message>
+        <source>If you have forgotten your password we can generate a new one for you. All you need to do is to enter your email address and we will create a new password for you.</source>
+        <translation>Se hai dimenticato la tua password possiamo generartene un&apos;altra. Devi solo inserire il tuo indirizzo email e noi ti creeremo una nuova password.</translation>
+    </message>
+    <message>
+        <source>Email</source>
+        <translation>Email</translation>
+    </message>
+    <message>
+        <source>Generate new password</source>
+        <translation>Genera una nuova password</translation>
+    </message>
+    <message>
+        <source>%siteurl forgot password</source>
+        <translation>%siteurl password dimenticata</translation>
+    </message>
+    <message>
+        <source>This link is valid until %1, after that time you should generate a new one.</source>
+        <translation>Questo link è valido fino a %1, dopo questo termine dovrai generarne un&apos;altra.</translation>
+    </message>
+</context>
+</TS>

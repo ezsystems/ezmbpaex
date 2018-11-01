a097f2bd2c4d5cf2b4e91afb8e9c694e58f0d1fb
Ivan Ternovtsiy <diaborn19@gmail.com>
2018-11-01 15:14:38 +0200

    FIX Last Visit date not updated after user logged
    We have developed custom cronjob to disable user who not logged in after some time after creating (by checking `ezuservisit` table).
    But, Last Visit date not updated if user logged in providing old and new passwords in this module (userpaex/password).
    I believe it should be updated in the same way as in standard "user/login" module.

--- modules/userpaex/password.php
+++ modules/userpaex/password.php
@@ -110,6 +110,7 @@ if ( $http->hasPostVariable( "OKButton" ) && $user)
                         $oldPassword = '';
 
                         eZUser::setCurrentlyLoggedInUser( $user, $UserID );
+                        eZUser::updateLastVisit( $UserID );
 
                         if ( $http->hasPostVariable( "RedirectOnChange" ) )
                         {

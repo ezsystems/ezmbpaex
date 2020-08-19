56b375ba656098686ca2e35bf619d09a253dd44f
Andreas Adelsberger <andreas.adelsberger@styleflasher.at>
2020-08-19 09:12:30 +0000

    fixed constructor

--- login_handler/ezpaexuser.php
+++ login_handler/ezpaexuser.php
@@ -40,7 +40,7 @@ class eZPaExUser extends eZUser
      */
     public function __construct( $row = null )
     {
-        parent::eZUser( $row );
+        parent::__construct( $row );
     }
 
     /**
